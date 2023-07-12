e204f867dad096081459519ddee1350479ca5cf4
hamza <hamzacoskun41@gmail.com>
2023-07-12 21:52:54 +0300

    chore: remove object files.

--- test.c
+++
@@ -1,8 +0,0 @@
-#include "ft_printf.h"
-#include <stdio.h>
-int main() {
-    printf("========================= ORIGINAL ========================\n");
-    printf("Returned: %d\n", printf("%p", ""));
-    printf("======================== FOURTY TWO ========================\n");
-    printf("Returned: %d\n", ft_printf("%p", ""));
-}
